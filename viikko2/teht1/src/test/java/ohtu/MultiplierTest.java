eebf5b2b5e75fbc545bce4d8be3e7dc6b0ca315e
Kevhann <kevin.hann@helsinki.fi>
2019-11-06 13:37:50 +0200

    tehtävät 1-3

---
+++ viikko2/teht1/src/test/java/ohtu/MultiplierTest.java
@@ -0,0 +1,18 @@
+package test.java.ohtu;
+
+import main.java.ohtu.Multiplier;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MultiplierTest {
+
+  @Test
+  public void kertominenToimii() {
+    Multiplier viisi = new Multiplier(5);
+
+    assertEquals(5, viisi.multipliedBy(1));
+    assertEquals(35, viisi.multipliedBy(7));
+  }
+
+}
